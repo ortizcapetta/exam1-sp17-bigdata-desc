43fd2a2bb15cf751d5efd1e23627c7c37a330627
ortizcapetta <alemariortiz@yahoo.com>
2018-10-24 11:12:01 -0400

    finished spark

--- spark/p3a.py
+++ spark/p3a.py
@@ -1,1 +1,11 @@
-#Code here
+from pyspark.sql import SparkSession
+from pyspark.sql import SQLContext
+spark = SparkSession.builder.getOrCreate()
+students = spark.read.csv('/home/alejandra/studentsPR.csv')
+school = spark.read.csv('/home/alejandra/escuelasPR.csv')
+students.createOrReplaceTempView("students")
+school.createOrReplaceTempView("school")
+result = spark.sql("Select students._c6 from students, school where students._c2 = school._c3 "
+                   "and school._c2 ='Ponce' or school._c2 = 'San Juan' and school._c4 ='Superior' and students._c5 ='M'")
+result.show()
+
--- spark/p3b.py
+++ spark/p3b.py
@@ -1,1 +1,11 @@
-#Code here
+from pyspark.sql import SparkSession
+from pyspark.sql import SQLContext
+spark = SparkSession.builder.getOrCreate()
+school = spark.read.csv('/home/alejandra/escuelasPR.csv')
+school.createOrReplaceTempView("school")
+schools = spark.sql("select _c1 as district, _c2 as city, count(*) "
+                    "from school "
+                    "where _c0 == 'Arecibo' "
+                    "group by district, city ")
+
+schools.show()
